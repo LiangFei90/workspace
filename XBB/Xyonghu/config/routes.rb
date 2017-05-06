cab5106730b56a423e88ac3d1a0e860e8d1aaf28
maodoum <1838537410@qq.com>
2017-05-06 21:27:29 +0800

    yonghu

--- XBB/Xyonghu/config/routes.rb
+++ XBB/Xyonghu/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :products
+  resources :categories do 
+    resources :products
+  end
+  resources :cities
+  resources :price_rules
+  resources :prices
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
